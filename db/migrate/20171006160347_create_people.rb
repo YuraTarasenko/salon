6f7021e585c049297722b30b5702a81c9a5121b0
Yurka <tarasenkoum@gmail.com>
2017-10-06 20:47:09 +0300

    com1

---
+++ db/migrate/20171006160347_create_people.rb
@@ -0,0 +1,11 @@
+class CreatePeople < ActiveRecord::Migration[5.1]
+  def change
+    create_table :people do |t|
+      t.string :name
+      t.text :bio
+      t.string :schedule
+
+      t.timestamps
+    end
+  end
+end
